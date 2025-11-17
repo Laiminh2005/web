22046ceb043c17e58acb5d0da0b6f7c9a9b78e2b
LAI MINH <minhlai20102005@gmail.com>
2025-11-17 09:41:38 +0700

    quen chua luu

---
+++ public/login.js
@@ -0,0 +1,91 @@
+import {auth, db} from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { collection, addDoc } from "firebase/firestore"; 
+
+const inpUserName = document.querySelector("#name");
+const inpEmail = document.querySelector("#email");
+const inpPass = document.querySelector("#password");
+
+const Dangky = document.querySelector("#or");
+
+const handlelogin = function (event)
+{
+    event.preventDefault();
+    let name = inpUserName.value;
+    let email = inpEmail.value;
+    let password = inpPass.value;
+
+    let role_id = 2;
+
+    if (email === ""){
+       alert("Email không được để trống.");
+        return;
+    }
+
+    else if(!email.includes("@") || !email.endsWith(".com")){
+       alert("email chưa đúng định dạng");
+       return;
+    }
+
+    //password
+    // Regex kiểm tra có ký tự đặc biệt
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+    if (password === ""){
+        alert("password không được để trống.");
+        return;
+    }
+
+    else if (password.length < 6) {
+        alert("Mật khẩu phải có ít nhất 6 ký tự.");
+        return;
+    }
+
+
+    else if(!specialCharRegex.test(password)){
+        alert("mật khẩu phải chứa ít nhất 1 ký tự đặc biệt");
+        return;
+    }
+
+    else if(!/[a-z]/.test(password)){
+        alert("mật khâu phải có chữ cái thường");
+        return;
+    }
+
+    else if(!/[A-Z]/.test(password)){
+        alert("mật khẩu phải có chữ hoa");
+        return;
+    }
+
+    else if(!/[0-9]/.test(password)){
+        alert("mật khẩu phải có số");
+        return;
+    }
+
+    //kiểm tra tên
+    if (name === "") {
+        alert("Vui lòng nhập họ tên.");
+        return;
+    }
+
+    createUserWithEmailAndPassword(auth, email,password)
+    .then ((userCredential)=>{
+        const user = userCredential.user;
+        const userData = {
+            Name,
+            email,
+            password,
+            role_id,
+            point: 0,
+        }
+        return addDoc(collection(db,"users"), userData)
+    })
+    .then(()=>{
+        alert("Đăng ký thành công");
+        window.location.href = "dangnhap.html";
+    })
+    .catch((e)=>{
+        alert("lỗi " +e.message);
+    })
+}
+
+or.addEventListener('submit', handlelogin);
